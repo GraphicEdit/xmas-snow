d3aa45240b401785e42e7736f3ac25971808b869
GraphicEdit <contact@graphicedit.com>
2013-01-14 12:48:52 +0000

    first

---
+++ readme.txt
@@ -0,0 +1,42 @@
+=== Xmas Snow ===
+
+Contributors: graphicedit
+Donate link: http://www.graphicedit.com/
+Tags: Christmas Snow, Christmas, winter, xhristmas Snow, Xmas, 
+Requires at least: 3.0
+Tested up to: 3.0
+Stable tag: 1.0.1
+
+Add nice looking animated Xhristmas(Christmas) Snow to the top of site.
+
+== Description ==
+
+Add nice looking animated Xhristmas(Christmas) Snow to the top of site. 
+
+Links: [Author Homepage](http://www.graphicedit.com/)
+
+== Installation ==
+
+1. Upload the "Xmas Snow" folder to the `/wp-content/plugins/` directory
+2. Activate the plugin through the 'Plugins' menu in WordPress
+3. and Done
+
+== Frequently Asked Questions ==
+not yet
+
+== Screenshots ==
+1. Xmas Snow.
+
+
+== Upgrade Notice ==
+not yet
+
+== Changelog ==
+1.0.1
+Add new color
+
+
+= 1.0.0 =
+* Initial release
+
+ 
---
+++ xmas-snow.php
@@ -0,0 +1,138 @@
+<?php
+/*
+Plugin Name: Xmas Snow
+Plugin URI: http://graphicedit.com/blog/plugin/xmas-snow/
+Description: This plugin adds Xmas Snow.
+Version: 1.0.1
+Author: GraphicEdit
+Author URI: http://www.graphicedit.com/
+
+	Copyright 2011  GraphicEdit  (email : contact@graphicedit.com)
+
+    This program is free software; you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation; either version 2 of the License, or
+    (at your option) any later version.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with this program; if not, write to the Free Software
+    Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
+*/
+
+
+function xmas_snow() {
+?>
+
+<!-- xmas_snow [ start ] -->
+<script type="text/javascript">
+// Set the number of snowflakes (more than 30 - 40 not recommended)
+var snowmax=50
+// Set the colors for the snow. Add as many colors as you like
+var snowcolor=new Array("#aaaacc","#ddddff","#ccccdd","#f3f3f3","#f0ffff","#bbf7f9")
+// Set the fonts, that create the snowflakes. Add as many fonts as you like
+var snowtype=new Array("Times","Arial","Times","Verdana")
+// Set the letter that creates your snowflake (recommended: * )
+var snowletter="*"
+// Set the speed of sinking (recommended values range from 0.3 to 2)
+var sinkspeed=0.6
+// Set the maximum-size of your snowflakes
+var snowmaxsize=30
+// Set the minimal-size of your snowflakes
+var snowminsize=8
+// Set the snowing-zone
+// Set 1 for all-over-snowing, set 2 for left-side-snowing
+// Set 3 for center-snowing, set 4 for right-side-snowing
+var snowingzone=1
+///////////////////////////////////////////////////////////////////////////
+var snow=new Array()
+var marginbottom
+var marginright
+var timer
+var i_snow=0
+var x_mv=new Array();
+var crds=new Array();
+var lftrght=new Array();
+var browserinfos=navigator.userAgent
+var ie5=document.all&&document.getElementById&&!browserinfos.match(/Opera/)
+var ns6=document.getElementById&&!document.all
+var opera=browserinfos.match(/Opera/)
+var browserok=ie5||ns6||opera
+
+function randommaker(range) {
+        rand=Math.floor(range*Math.random())
+    return rand
+}
+
+function initsnow() {
+        if (ie5 || opera) {
+                marginbottom = document.body.scrollHeight
+                marginright = document.body.clientWidth-15
+        }
+        else if (ns6) {
+                marginbottom = document.body.scrollHeight
+                marginright = window.innerWidth-15
+        }
+        var snowsizerange=snowmaxsize-snowminsize
+        for (i=0;i<=snowmax;i++) {
+                crds[i] = 0;
+            lftrght[i] = Math.random()*15;
+            x_mv[i] = 0.03 + Math.random()/10;
+                snow[i]=document.getElementById("s"+i)
+                snow[i].style.fontFamily=snowtype[randommaker(snowtype.length)]
+                snow[i].size=randommaker(snowsizerange)+snowminsize
+                snow[i].style.fontSize=snow[i].size+'px';
+                snow[i].style.color=snowcolor[randommaker(snowcolor.length)]
+                snow[i].style.zIndex=1000
+                snow[i].sink=sinkspeed*snow[i].size/5
+                if (snowingzone==1) {snow[i].posx=randommaker(marginright-snow[i].size)}
+                if (snowingzone==2) {snow[i].posx=randommaker(marginright/2-snow[i].size)}
+                if (snowingzone==3) {snow[i].posx=randommaker(marginright/2-snow[i].size)+marginright/4}
+                if (snowingzone==4) {snow[i].posx=randommaker(marginright/2-snow[i].size)+marginright/2}
+                snow[i].posy=randommaker(2*marginbottom-marginbottom-2*snow[i].size)
+                snow[i].style.left=snow[i].posx+'px';
+                snow[i].style.top=snow[i].posy+'px';
+        }
+        movesnow()
+}
+
+function movesnow() {
+        for (i=0;i<=snowmax;i++) {
+                crds[i] += x_mv[i];
+                snow[i].posy+=snow[i].sink
+                snow[i].style.left=snow[i].posx+lftrght[i]*Math.sin(crds[i])+'px';
+                snow[i].style.top=snow[i].posy+'px';
+
+                if (snow[i].posy>=marginbottom-2*snow[i].size || parseInt(snow[i].style.left)>(marginright-3*lftrght[i])){
+                        if (snowingzone==1) {snow[i].posx=randommaker(marginright-snow[i].size)}
+                        if (snowingzone==2) {snow[i].posx=randommaker(marginright/2-snow[i].size)}
+                        if (snowingzone==3) {snow[i].posx=randommaker(marginright/2-snow[i].size)+marginright/4}
+                        if (snowingzone==4) {snow[i].posx=randommaker(marginright/2-snow[i].size)+marginright/2}
+                        snow[i].posy=0
+                }
+        }
+        var timer=setTimeout("movesnow()",50)
+}
+
+for (i=0;i<=snowmax;i++) {
+        document.write("<span id='s"+i+"' style='position:absolute;top:-"+snowmaxsize+"'>"+snowletter+"</span>")
+}
+if (browserok) {
+        window.onload=initsnow
+}
+</script>
+
+<!-- xmas_snow [ end ] -->
+<?php   
+}
+
+
+//////////////////
+/////////////////
+add_action('wp_footer','xmas_snow'); 
+
+?>
